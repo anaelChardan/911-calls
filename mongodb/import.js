2a8736dbfa61d18a01ae520ad3f5c6363f07ac8d
Romain Davaze <davaze.romain@gmail.com>
2018-02-20 13:28:42 +0100

    :art: Refactor and clean code of import

--- mongodb/import.js
+++ mongodb/import.js
@@ -1,43 +1,42 @@
-var mongodb = require('mongodb');
-var csv = require('csv-parser');
-var fs = require('fs');
+const mongodb = require('mongodb');
+const csv = require('csv-parser');
+const fs = require('fs');
 
-var MongoClient = mongodb.MongoClient;
-var mongoUrl = 'mongodb://localhost:27017/911-calls';
+const MongoClient = mongodb.MongoClient;
+const mongoUrl = 'mongodb://localhost:27017/911-calls';
 
+const transformData = data => {
+    // We're not taking desc and e fields as they're useless
+    const { lat: latitude, lng: longitude, zip: zipCode, title, timeStamp: date, twp: neighbourhood, addr: address } = data;
 
+    const delimiterIndex = title.indexOf(':');
 
-var insertCalls = function(db, callback) {
-    var collection = db.collection('calls');
+    const category = title.substring(0, delimiterIndex);
+    const event = title.substring(delimiterIndex + 1, title.length).trim();
 
-    var calls = [];
+    return {
+        coordinates: [parseFloat(latitude), parseFloat(longitude)],
+        zipCode,
+        category,
+        event,
+        date,
+        neighbourhood,
+        address
+    };
+}
+
+const insertCalls = (db, callback) => {
+    const collection = db.collection('calls');
+
+    const calls = [];
     fs.createReadStream('../911.csv')
         .pipe(csv())
-        .on('data', data => {
-            // On prend pas la description et le e car useless
-            const { lat: latitude, lng: longitude, zip: zipCode, title, timeStamp: date, twp : neighbourhood, addr: address } = data;
-
-            const delimiterIndex = title.indexOf(':');
-            const category = title.substring(0, delimiterIndex);
-            
-            const event = title.substring(delimiterIndex + 1, title.length).trim();
-
-            var call = {
-                coordinates : [parseFloat(latitude), parseFloat(longitude)],
-                zipCode,
-                category,
-                event,
-                date,
-                neighbourhood,
-                address
-            };
-
-            calls.push(call);
-        })
+        .on('data', data => calls.push(transformData(data)))
         .on('end', () => {
-          collection.insertMany(calls, (err, result) => {
-            callback(result)
-          });
+            collection.insertMany(calls, (err, result) => {
+                if (err) console.log(err)
+                else callback(result)
+            });
         });
 }
 
